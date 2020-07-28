a2292e5f188854f015ff8e8c96c76e27323f050a
cylim <cylim226@gmail.com>
2020-07-28 10:16:37 +0800

    Initial Commit

---
+++ testProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testProject
+//
+//  Created by Chee Yeong Lim on 7/28/20.
+//  Copyright © 2020 Chee Yeong Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
